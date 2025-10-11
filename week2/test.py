76e122f8ab49dc98b69a0ff84b307e71a20124c1
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-10-10 22:44:57 -0500

    20251010_3

--- week2/test.py
+++ week2/test.py
@@ -10,21 +10,20 @@ def greating_function(name, extra_word):
 
 def greating_function(name, extra_word):
     print("hello, %s. Welcome and %s"%(name,extra_word))
-
+"""
 def sum_two_numbers(x, y):
     return x + y
 
 def multiply_two_numbers(x, y):
     return x*y
-
+"""
 
 greating_function("Nurzhan","bye bye")
 greating_function("Aizhan", "make your self comfortable")
 z=sum_two_numbers(9,7.5)
-print(z)
-we have to count 21*21 + 46*47
+print(z)"""
+# we have to count 21*21 + 46*47
 
 print(sum_two_numbers(multiply_two_numbers(21,21),multiply_two_numbers(46,47)))
-print(sum_two_numbers(multiply_two_numbers(21,21),multiply_two_numbers(46,47)))
-"""
-print("hello again")
+
+# print("hello again")
