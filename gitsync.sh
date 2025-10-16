cc04631e63cb7d5bb0deeac1f31921a5d6d2c4e9
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-10-16 00:11:39 -0500

    2025-10-16_00:11:39

--- gitsync.sh
+++ gitsync.sh
@@ -1,5 +1,29 @@
-#!usr/bin/bash/
-set -e
-echo "----$(date) ----"
-cd /c/Users/NRG/GH  || exit 1
+#!/bin/bash
 
+LOG_FILE="/c/Users/NRG/Gitlog.txt"
+echo "$(date): запуск" >> LOG_FILE
+cd /c/Users/NRG/GH
+
+if [ -n "$(git status --porcelain)" ]; then
+    if git add . >> "$LOG_FILE" 2>&1; then
+        echo "Файлы добавлены"
+    else
+        echo "ОШИБКА при git add" >> "$LOG_FILE"
+        exit 1
+    fi
+    if git commit -m "$(date '+%Y-%m-%d_%H:%M:%S')" >> "$LOG_FILE" 2>&1; then
+        echo "Коммит создан"
+    else
+        echo "ОШИБКА при создании коммита" >> "$LOG_FILE"
+        exit 1
+    fi
+    if git push origin main >> "$LOG_FILE" 2>&1; then
+        echo "$(date): Изменения отправлены в GitHub" >> "$LOG_FILE"
+    else
+        echo "$(date): ОШИБКА при отправке" >> "$LOG_FILE"
+    fi
+else
+    echo "$(date): Изменений нет" >> LOG_FILE
+fi
+
+echo "---" >> LOG_FILE
