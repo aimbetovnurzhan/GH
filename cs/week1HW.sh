7b14f11493620e6e5b0caffafad31554dbc8dae2
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-10-21 00:00:01 -0500

    2025-10-21_00:00:01

--- cs/week1HW.sh
+++ cs/week1HW.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#curl https://www.usefy.com/ -o /c/Users/NRG/GH/CS/week1HwRes.txt
-#curl https://www.usefy.com/ | sed 's/<[^>]*>//g' > /c/Users/NRG/GH/CS/week1HwRes.txt
+start="AI-Powered Learning"
+end="2025 Usefy"
+outfile="/c/Users/NRG/GH/CS/week1HwRes.txt"
 
-curl https://www.usefy.com/ | \
-  sed 's/<[^>]*>//g' | \
-  sed 's/&[a-z]*;/ /g' | \
-  sed 's/^[[:space:]]*//g' | \
-  sed '/^$/d' | \
-  grep -i -E "(usefy|course|learning|organization|AI|platform|video)" \
-  > /c/Users/NRG/GH/CS/week1HwRes.txt
+curl https://www.usefy.com/ \
+| sed 's/<[^>]*>//g' \
+| sed -n "/$start/,/$end/p" \
+> "$outfile"
