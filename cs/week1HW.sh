b16225a482055e84864730bad8c12cfa76afc2a8
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-10-20 23:12:00 -0500

    2025-10-20_23:12:00

--- cs/week1HW.sh
+++ cs/week1HW.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl https://www.usefy.com/ -o /c/Users/NRG/GH/CS/week1HwRes.txt
+#curl https://www.usefy.com/ -o /c/Users/NRG/GH/CS/week1HwRes.txt
+#curl https://www.usefy.com/ | sed 's/<[^>]*>//g' > /c/Users/NRG/GH/CS/week1HwRes.txt
+
+curl https://www.usefy.com/ | \
+  sed 's/<[^>]*>//g' | \
+  sed 's/&[a-z]*;/ /g' | \
+  sed 's/^[[:space:]]*//g' | \
+  sed '/^$/d' | \
+  grep -i -E "(usefy|course|learning|organization|AI|platform|video)" \
+  > /c/Users/NRG/GH/CS/week1HwRes.txt
