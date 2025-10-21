f2e5fa010405390679549ad6820246f3d9593f6e
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-10-21 00:12:00 -0500

    2025-10-21_00:12:00

--- cs/week1HW.sh
+++ cs/week1HW.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-start="AI-Powered Learning"
-end="2025 Usefy"
+start="colorScheme"
+end="Quick LinksLegalConnect"
 outfile="/c/Users/NRG/GH/CS/week1HwRes.txt"
 
 curl https://www.usefy.com/ \
 | sed 's/<[^>]*>//g' \
-| sed -n "/$start/,/$end/p" \
+| perl -0777 -ne "print \$1 if /$start(.*?)$end/s" \
 > "$outfile"
+
