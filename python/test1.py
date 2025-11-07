f538ef58bb94b15a729adafdc46dbb0864506e36
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-11-07 04:00:02 -0600

    2025-11-07_04:00:02

--- python/test1.py
+++ python/test1.py
@@ -1,32 +1,52 @@
 #!/usr/bin/python3
 import sys
-testArgv = ['test1.py', '--count', 'c:/Users/NRG/GH/python/small.txt']
+testArgv = ['test1.py'\
+            , '--count'\
+            , 'c:/Users/NRG/GH/python/alice.txt']
 
 def file_parser(file_name):
     with open(file_name, 'r') as fn:
-        text = fn.read().replace("\n", " ").replace(".", "").replace(",", "").split(" ")
+        text = fn.read().lower()\
+                 .replace("\n", " ")\
+                 .replace(".", " ")\
+                 .replace(",", " ")\
+                 .replace("?", " ")\
+                 .replace("!", " ")\
+                 .replace("-", " ")\
+                 .replace(":", " ")\
+                 .replace(";", " ")\
+                 .replace('"', " ")\
+                 .replace("`", " ")\
+                 .replace("'", " ")\
+                 .replace(")", " ")\
+                 .replace("(", " ")\
+                 .replace("]", " ")\
+                 .replace("[", " ")\
+                 .replace("_", " ")\
+                 .replace("*", " ")\
+                 .split()
         word_cnt = {}
         for word in text:
-            word_cnt[word] =  word_cnt.get(word, 0) + 1
+            if word.isdigit() is False and len(word) == 2:
+                word_cnt[word] =  word_cnt.get(word, 0) + 1
     return word_cnt
 
 def print_words(file_name):
-    print(file_parser(file_name))
+    print(sorted(file_parser(file_name).items()))
     
 def print_top(file_name):
-    print(file_name)
+    print(sorted(file_parser(file_name).items(), key = lambda x: x[1], reverse = True)[:20])
         
 def main():
-#   if len(sys.argv) != 3:
-  if len(testArgv) != 3:
+  if len(sys.argv) != 3:
+  # if len(testArgv) != 3:
     print('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
-#   option = sys.argv[1]
-#   filename = sys.argv[2]
-  option = testArgv[1]
-  filename = testArgv[2]
-
+  option = sys.argv[1]
+  filename = sys.argv[2]
+  # option = testArgv[1]
+  # filename = testArgv[2]
 
   if option == '--count':
     print_words(filename)
@@ -38,4 +58,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-  
