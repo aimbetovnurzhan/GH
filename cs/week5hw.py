c0eb2d446592d84d2bf873b7a6d91440a2746ced
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-11-11 07:22:29 -0600

    2025-11-11_07:22:29

--- cs/week5hw.py
+++ cs/week5hw.py
@@ -1,8 +1,22 @@
 import random as rd
 
+def check_attempt(guessing_number, attempt):
+    if attempt > guessing_number:
+        if attempt - guessing_number > 5:
+            return
+            print("Too high")
+        else:
+            print("Little high")
+    elif attempt < guessing_number:
+        if guessing_number - attempt > 5:
+            print("Too low")
+        else:
+            print("Little low")
+    else:
+        print("Nice job, correct, you won! ))")
 
 a = rd.randint(1, 100)
-attempts = 5
+attempts_cnt = 5
 print("Gues the number, you have %d attempts to guess it" %(attempts)) #, a)
 
 for attempt in range(attempts):
