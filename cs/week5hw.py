9e3eb157b389a3d8f0c2511cdb255f31882a0191
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-11-14 06:00:02 -0600

    2025-11-14_06:00:02

--- cs/week5hw.py
+++ cs/week5hw.py
@@ -2,23 +2,24 @@
 
 import random as rd
 
+min_val = 1
 default_attempts_cnt = 5
 max_attempts_cnt = 10
 max_guessing_number_val = 100
 guessing_number = rd.randint(1, max_guessing_number_val)
 answers = {-2: "Too Low", -1: "Little low", 0: "You guessed it, great job", 1: "Little high", 2: "Too high", }
 
-def check_user_input(is_attempt): # should return number between (1 and 100) for True or (1 and 10) for False or 0 for break  
+def check_user_input(is_attempt): # returns number in range 0 to 100/10 or 0 (user_exit/defaultval) for True/False.  
     max_val = max_attempts_cnt
-    break_msg = "to continue with %d attempts" %default_attempts_cnt
+    break_msg = "to continue with %d attempts: " %default_attempts_cnt
     if is_attempt:
         max_val = max_guessing_number_val
-        break_msg = "if you're giving up"
+        break_msg = "if you're giving up: "
     while True:
         user_input = input("Enter a number or press 'Enter' %s" %break_msg)
         if user_input.isdigit():
             user_input = int(user_input)
-            if 0 < user_input < max_val:
+            if min_val <= user_input <= max_val:
                 return user_input
             else:
                 print("Number should be between 1 and %d" %max_val)
@@ -51,9 +52,10 @@ def main():
 
     for i in range(1, attempts_cnt + 1):
         print("Attempt #%d" %i)
-        attempt = check_attempt(True)
-            if attempt == 0:
-                return "L_user gave up xD"
+        attempt = check_user_input(True)
+        if attempt == 0:
+            print("L_user gave up, see ya xD")
+            return
         attempt_res = check_attempt(guessing_number, attempt)
         print(answers[attempt_res])
         if attempt_res == 0:
@@ -63,3 +65,37 @@ def main():
 
 if __name__ == '__main__':
   main()
+
+
+
+"""
+def main():
+  print('donuts')
+  # Each line calls donuts, compares its result to the expected for that call.
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print()
+  print('both_ends')
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+
+  print()
+  print('fix_start')
+  test(fix_start('babble'), 'ba**le')
+  test(fix_start('aardvark'), 'a*rdv*rk')
+  test(fix_start('google'), 'goo*le')
+  test(fix_start('donut'), 'donut')
+
+  print()
+  print('mix_up')
+  test(mix_up('mix', 'pod'), 'pox mid')
+  test(mix_up('dog', 'dinner'), 'dig donner')
+  test(mix_up('gnash', 'sport'), 'spash gnort')
+  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+"""
