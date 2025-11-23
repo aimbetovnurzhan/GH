a802640840d3c72bddd435a63588271bca38f35e
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-11-23 05:28:13 -0600

    2025-11-23_05:28:13

--- cs/week8/client.py
+++ cs/week8/client.py
@@ -6,6 +6,11 @@ port = 65432
 
 s.connect(("127.0.0.1", port))
 
-print(s.recv(1024).decode())
+while True:
+    msg = input("Введите сообщение (или 'exit'): ")
+    if msg == "exit":
+        break
+    s.sendall(msg.encode())
 
+print(s.recv(1024).decode())
 s.close()
--- cs/week8/server.py
+++ cs/week8/server.py
@@ -1,21 +1,24 @@
 import socket
 
 s = socket.socket()
-print("Socket successfully created")
+print("Socket created")
 
 port = 65432
 
 s.bind(("", port))
-print("Socket binded to %s" % (port))
+print("Socket binded to port %s" % (port))
 
-s.listen(5)
-print("Socket is listening")
+s.listen()
+print("listening...")
 
 while True:
-    c, addr = s.accept()
-    print("Got connection from ", c, addr)
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    print("Got connection from ", addr)
+    conn.send("Thanks for connection ".encode())
+    print("Получено:", data.decode())
+    conn.send("Принято: ", data)
 
-    c.send("Thanks for connection ".encode())
     c.close()
 
     break
