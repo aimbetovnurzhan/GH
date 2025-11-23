ecafa05da356f7af25bfdd7260eb6ecef471193b
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-11-23 04:25:39 -0600

    2025-11-23_04:25:39

--- cs/week8/server.py
+++ cs/week8/server.py
@@ -1,1 +1,19 @@
-  asdf 
+import socket
+
+s = socket.socket()
+print("Socket successfully created")
+
+port = 65432
+
+s.bind(("", port))
+print("Socket binded to %s" % (port))
+
+s.listen(5)
+print("Socket is listening")
+
+while True:
+    c, addr = s.accept()
+    print("Got connection form ", addr)
+
+    c.send("Thanks for connection ".encode())
+    c.close()
