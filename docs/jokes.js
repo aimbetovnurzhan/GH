18387a3b9cd6b4c1370e0749e0c77d596589c4c3
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-11-28 08:40:39 -0600

    2025-11-28_08:40:39

--- docs/jokes.js
+++ docs/jokes.js
@@ -2,25 +2,26 @@ async function getJoke() {
   const jokeRes = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
   const jokeData = await jokeRes.json();
   const joke = jokeData.joke || `${jokeData.setup} — ${jokeData.delivery}`;
+
   document.getElementById("original").textContent = joke;
 
-    async function translate(text, source = "en", target = "ru") {
-    const url = "https://api.mymemory.translated.net/get";
-    const params = new URLSearchParams({
-        q: text,
-        langpair: `${source}|${target}`
-    });
+  const translated = await translate(joke, "en", "ru");
+  document.getElementById("translated").textContent = translated;
+}
 
-    try {
-        const response = await fetch(`${url}?${params}`);
-        const data = await response.json();
-        return data.responseData.translatedText;
-    } catch (error) {
-        console.error("Ошибка перевода:", error);
-        return "Ошибка";
-    }
-    };
+async function translate(text, source = "en", target = "ru") {
+  const url = "https://api.mymemory.translated.net/get";
+  const params = new URLSearchParams({
+    q: text,
+    langpair: `${source}|${target}`
+  });
 
-  const translated = await translateRes.json();
-  document.getElementById("translated").textContent = translated.translatedText || "Translate error";
+  try {
+    const res = await fetch(`${url}?${params}`);
+    const data = await res.json();
+    return data.responseData.translatedText || "Ошибка перевода";
+  } catch (err) {
+    console.error("Ошибка перевода:", err);
+    return "Ошибка соединения";
+  }
 }
