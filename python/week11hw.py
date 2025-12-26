967d6930837b7ba7fc118f93e7eb29c7ea03d8a3
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2025-12-26 09:40:33 -0600

    2025-12-26_09:40:33

--- python/week11hw.py
+++ python/week11hw.py
@@ -1,14 +1,19 @@
-target = 5
-data_list = [1, 2, 3, 4, 5]
+from random import randint as rd
+
+inp_target = rd(1,10)
+inp_list = []
+for i in range (10):
+    inp_list.append(rd(1,10))
+print(f"list for input {inp_list}\ntarget for search = {inp_target}")
 
 def linear_search(data_list, target):
-    for i in len(data_list):
+    for i in range(len(data_list)):
         if data_list[i] == target:
             return i
     return -1
 
 def main():
-    print(linear_search(data_list, target))
+    print(linear_search(inp_list, inp_target))
 
 if __name__ == "__main__":
     main()
