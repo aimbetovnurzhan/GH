46c1c24a107120762e837f1ab66c538fb70e0785
aimbetovnurzhan <aymbetovnurzhan@gmail.com>
2026-01-07 22:00:03 -0600

    2026-01-07_22:00:03

--- cs/week10/src/services/workoutService.js
+++ cs/week10/src/services/workoutService.js
@@ -18,6 +18,7 @@ const createNewWorkout = (newWorkout) => {
     createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
     updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
   };
+  
   try {
     const createdWorkout = Workout.createNewWorkout(workoutToInsert);
     return createdWorkout;
