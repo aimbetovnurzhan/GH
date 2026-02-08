bc05420b213149bdb69d95053c68b556dad88e71
Nurzhan <aymbetovnurzhan@gmail.com>
2026-02-08 06:18:16 +0500

    Time: 15 ms (5.05%), Space: 12.5 MB (22.84%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def addBinary(self, a, b):
         mx, mn = list(a[::-1]), list(b[::-1])
-        i, j = len(a), len(b)
         if len(b) >= len(a):
             mx, mn = mn, mx
-            i, j = j, i
-        for k in range(i):
-            if k < j:
-                if k+1 < i:
+        for k in range(len(mx)):
+            if k < len(mn):
+                if k+1 < len(mx):
                     mx[k + 1] = (int(mx[k + 1]) + (int(mx[k]) + int(mn[k])) // 2)
                 else:
                     if (int(mx[k]) + int(mn[k])) // 2 > 0:
@@ -15,7 +13,7 @@ class Solution(object):
                 mx[k] = (int(mx[k]) + int(mn[k])) % 2
             else:
                 if int(mx[k]) // 2 > 0:
-                    if k+1 < i:
+                    if k+1 < len(mx):
                         mx[k + 1] = int(mx[k + 1]) + int(mx[k]) // 2
                     else:
                         mx.append(int(mx[k]) // 2)
