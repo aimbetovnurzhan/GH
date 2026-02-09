50d5eba557f89e744906f0b58744dd1774df5392
Nurzhan <aymbetovnurzhan@gmail.com>
2026-02-09 17:10:35 +0500

    Time: 6 ms (9.3%), Space: 46.9 MB (28.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,35 +1,13 @@
-import java.util.HashMap;
-import static java.util.Map.entry;
-
 class Solution {
     public int romanToInt(String s) {
-    Map<String, Integer> dgts = Map.ofEntries(
-        entry("I", 1),
-        entry("V", 5),
-        entry("X", 10),
-        entry("L", 50),
-        entry("C", 100),
-        entry("D", 500),
-        entry("M", 1000),
-        entry("IV", 4),
-        entry("IX", 9),
-        entry("XL", 40),
-        entry("XC", 90),
-        entry("CD", 400),
-        entry("CM", 900)
-    );
-
-        int i = 0;
+    Map<Character, Integer> dgts = Map.of('I',1,'V',5,'X',10,'L',50,'C',100,'D',500,'M',1000);
         int sum = 0;
-        String[] ltrs = s.split("");
-        while (i < ltrs.length) {
-            // System.out.println("Ищу ключ: '" + ltrs[i] + "'");
-            if ((i + 1 < ltrs.length) && (dgts.get(ltrs[i] + ltrs[i + 1]) != null)) {
-                sum = sum + dgts.get(ltrs[i] + ltrs[i + 1]);
-                i += 2;
+        for (int i = 0; i < s.length(); i++) {
+            // System.out.println("Ищу ключ: '" + s.charAt(i) + "'");
+            if (i + 1 < s.length() && dgts.get(s.charAt(i)) < dgts.get(s.charAt(i + 1))) {
+                sum -= dgts.get(s.charAt(i));
             } else {
-                sum = sum + dgts.get(ltrs[i]);
-                i ++;
+                sum += dgts.get(s.charAt(i));
             }
         }
         return sum;
