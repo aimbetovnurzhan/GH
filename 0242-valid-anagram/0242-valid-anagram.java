2672d4426951a7f5a52f8edd2531e06e5fc787b1
Nurzhan <aymbetovnurzhan@gmail.com>
2026-02-22 22:08:52 +0500

    Time: 14 ms (26.36%), Space: 46.5 MB (33.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+        for (char c: t.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0) - 1);
+        }
+        map.entrySet().removeIf(entry -> entry.getValue() == 0);
+        if (map.isEmpty()) return true;
+        return false;
+    }
+}
